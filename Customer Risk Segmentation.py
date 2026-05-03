3d8c43dabd97ef68eb963a56f2209e75d4334d23
marcuslee6 <93899705+marcuslee6@users.noreply.github.com>
2026-05-03 21:04:28 +0800

    Update Customer Risk Segmentation.py

--- Customer Risk Segmentation.py
+++ Customer Risk Segmentation.py
@@ -1,31 +1,54 @@
 import pandas as pd
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as PLT
 import seaborn as sns
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from scipy.cluster.hierarchy import dendrogram, linkage
 
+#1. Load Data
 df = pd.read_csv('insurance.csv')
 
+#2 Data Preprocessing: Select 'Age', 'BMI' and 'Charges' to divide customers
 category=['age','bmi','charges']
 x=df[category]
-
 scale = StandardScaler()
 x_scale = scale.fit_transform(x)
 
-plot.figure(figsize=(10,7))
-plot.title('Customer Hierarchical Dendrogram')
-plot.xlabel('Number of Clusters')
-plot.ylabel('within-cluster sum of squares error')
-plot.show()
+#3 Use agglomerative hierarchical clustering to minimize variance within clusters
+PLT.figure(figsize=(10,7))
+PLT.title('Customer Hierarchical Dendrogram')
+#for large datasets, we truncate the tree to make it readable
+linkage_matrix = linkage(x_scale, method='ward')
+dendrogram(linkage_matrix, truncate_mode='lastp', p=12)
+PLT.xlabel("Cluster size (or index)")
+PLT.ylabel("Distance")
+PLT.show()
 
+#4. K-Means Method
+WCSS = []
+for i in range(1, 11):
+  k_means = KMeans(n_clusters=i, init='k-means++', random_state=36)
+  k_means.fit(x_scale)
+  WCSS.append(k_means.inertia_)
+
+PLT.figure(figsize=(8, 5))
+PLT.plot(range(1, 11), WCSS, marker='o', linestyle='--')
+PLT.title('Elbow Method for Optimal K')
+PLT.xlabel('Number of Clusters')
+PLT.ylabel('within-cluster sum of squares error')
+PLT.show()
+
+#5 Final K-Means Execution (Choose K=4)
 k_means= KMeans(n_clusters=4, init='k-means++', random_state=36)
 df['Cluster'] = k_means.fit_predict(x_scale)
 
+#6 Result Interpretation
 print("Average attributes per cluster:")
 print(df.groupby('Cluster')[category].mean())
 
-plot.figure(figsize=(8,6))
-sns.scatterplot(data.df, x='bmi',y='charges', hue='cluster', palette='virdis')
-plot.title('Cluster Segments:BMI vs Charges')
-plot.show()
+#Visualize the final clusters
+PLT.figure(figsize=(8,6))
+sns.scatterplot(data=df, x='bmi',y='charges', hue='Cluster', palette='viridis')
+PLT.title('Cluster Segments:BMI vs Charges')
+PLT.show()
+
