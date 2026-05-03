4222c88e75bf3266cccbd385378603d1bb646263
marcuslee6 <93899705+marcuslee6@users.noreply.github.com>
2026-05-03 20:14:59 +0800

    Add files via upload

---
+++ Customer Risk Segmentation.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plot
+import seaborn as sns
+from sklearn.cluster import KMeans
+from sklearn.preprocessing import StandardScaler
+from scipy.cluster.hierarchy import dendrogram, linkage
+
+df = pd.read_csv('insurance.csv')
+
+category=['age','bmi','charges']
+x=df[category]
+
+scale = StandardScaler()
+x_scale = scale.fit_transform(x)
+
+plot.figure(figsize=(10,7))
+plot.title('Customer Hierarchical Dendrogram')
+plot.xlabel('Number of Clusters')
+plot.ylabel('within-cluster sum of squares error')
+plot.show()
+
+k_means= KMeans(n_clusters=4, init='k-means++', random_state=36)
+df['Cluster'] = k_means.fit_predict(x_scale)
+
+print("Average attributes per cluster:")
+print(df.groupby('Cluster')[category].mean())
+
+plot.figure(figsize=(8,6))
+sns.scatterplot(data.df, x='bmi',y='charges', hue='cluster', palette='virdis')
+plot.title('Cluster Segments:BMI vs Charges')
+plot.show()
